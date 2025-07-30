9046a58a9cce17a01bafcdeb71ed40150f83de3c
naim195 <naimkhan.shaikh@iitgn.ac.in>
2025-07-30 23:50:31 +0530

    added connectRemoteServer function to resolve hostname and establish TCP connection to remote server

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -42,6 +42,44 @@ pthread_mutex_t lock; // to lock cache
 cache_element *head;
 int cache_size;
 
+int connectRemoteServer(char* host_addr, int port_num)
+{
+	// create socket for remote server
+	int remoteSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if( remoteSocket < 0)
+	{
+		printf("Error in Creating Socket.\n");
+		return -1;
+	}
+	
+	// get host by the name/ip address provided
+	struct hostent *host = gethostbyname(host_addr);	
+	if(host == NULL)
+	{
+		fprintf(stderr, "No such host exists.\n");	
+		return -1;
+	}
+
+	// inserts ip address and port number of host in struct `server_addr`
+	struct sockaddr_in server_addr;
+
+	bzero((char*)&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port_num);
+
+	bcopy((char *)host->h_addr,(char *)&server_addr.sin_addr.s_addr,host->h_length);
+
+	// Connect to Remote server ----------------------------------------------------
+
+	if( connect(remoteSocket, (struct sockaddr*)&server_addr, (socklen_t)sizeof(server_addr)) < 0 )
+	{
+		fprintf(stderr, "Error in connecting !\n"); 
+		return -1;
+	}
+	// free(host_addr);
+	return remoteSocket;
+}
 
 int handle_request(int clientSocket, ParsedRequest *request, char *tempReq)
 {
