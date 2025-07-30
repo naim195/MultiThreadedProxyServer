30d73ed7dc96888b927cbdfaa8af6c0c01126c3f
naim195 <naimkhan.shaikh@iitgn.ac.in>
2025-07-30 09:32:13 +0530

    add thread_fn partially

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -15,6 +15,7 @@
 #include <errno.h>
 
 #define MAX_CLIENTS 10
+#define MAX_BYTES 4096
 
 typedef struct cache_element cache_element;
 
@@ -33,7 +34,7 @@ int add_cache_element(char *data, int size, char *url);
 void remove_cache_element();
 
 int port_number = 8080;
-int proxySocketId;
+int proxy_socketId;
 pthread_t tid[MAX_CLIENTS]; // thread ids array to store ids of clients
 sem_t semaphore;
 pthread_mutex_t lock; // to lock cache
@@ -41,10 +42,22 @@ pthread_mutex_t lock; // to lock cache
 cache_element *head;
 int cache_size;
 
+void *thread_fn(void* socketNew){
+    sem_wait(&semaphore);
+    int p;
+    sem_getvalue(&semaphore, p);
+    printf("Semaphore value is:%d\n", p);
+	int *t = (int *)socketNew;
+	int socket = *t;
+	int bytes_send_client, len;
+	char *buffer = (char *)calloc(MAX_BYTES, sizeof(char));
+	bzero(buffer, MAX_BYTES);
+}
+
 int main(int argc, char *argv[])
 {
     int client_socketId, client_len;
-    struct sockaddr server_addr, client_addr;
+    struct sockaddr_in server_addr, client_addr;
     sem_init(&semaphore, 0, MAX_CLIENTS);
     pthread_mutex_init(&lock, NULL);
     if(argv==2){
@@ -56,5 +69,74 @@ int main(int argc, char *argv[])
     }
 
     printf("Starting proxy server at port, %d\n", port_number);
-    
+
+     //creating the proxy socket
+	proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
+
+	if( proxy_socketId < 0)
+	{
+		perror("Failed to create socket.\n");
+		exit(1);
+	}
+
+	int reuse =1;
+	if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) 
+        perror("setsockopt(SO_REUSEADDR) failed\n");
+
+	bzero((char*)&server_addr, sizeof(server_addr));  
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port_number); // assigning port to the Proxy
+	server_addr.sin_addr.s_addr = INADDR_ANY; // any available address assigned
+
+    // bind the socket
+	if( bind(proxy_socketId, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0 )
+	{
+		perror("Port is not available\n");
+		exit(1);
+	}
+	printf("Binding on port: %d\n",port_number);
+
+    // Proxy socket listening to the requests
+	int listen_status = listen(proxy_socketId, MAX_CLIENTS);
+
+	if(listen_status < 0 )
+	{
+		perror("Error while Listening !\n");
+		exit(1);
+	}
+
+	int i = 0; // Iterator for thread_id (tid) and Accepted Client_Socket for each thread
+	int Connected_socketId[MAX_CLIENTS];   // This array stores socket descriptors of connected clients
+
+    // Infinite Loop for accepting connections
+	while(1)
+	{
+		
+		bzero((char*)&client_addr, sizeof(client_addr));			// Clears struct client_addr
+		client_len = sizeof(client_addr); 
+
+        // accepting the connections
+		client_socketId = accept(proxy_socketId, (struct sockaddr*)&client_addr,(socklen_t*)&client_len);	// Accepts connection
+		if(client_socketId < 0)
+		{
+			fprintf(stderr, "Unable to connect !\n");
+			exit(1);
+		}
+		else{
+			Connected_socketId[i] = client_socketId; // Storing accepted client into array
+		}
+
+		// Getting IP address and port number of client
+		struct sockaddr_in* client_pt = (struct sockaddr_in*)&client_addr;
+		struct in_addr ip_addr = client_pt->sin_addr;
+		
+		char str[INET_ADDRSTRLEN];  // INET_ADDRSTRLEN: Default ip address size
+		inet_ntop( AF_INET, &ip_addr, str, INET_ADDRSTRLEN );
+		printf("Client is connected with port number: %d and ip address: %s \n",ntohs(client_addr.sin_port), str);
+		//printf("Socket values of index %d in main function is %d\n",i, client_socketId);
+		pthread_create(&tid[i],NULL,thread_fn, (void*)&Connected_socketId[i]); // creating a thread for each client accepted
+		i++; 
+	}
+	close(proxy_socketId);									// close socket
+ 	return 0;
 }
